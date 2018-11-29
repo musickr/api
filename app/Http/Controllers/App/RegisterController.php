b150487221985206e3ccb2b5c5d9e7be72b20820
z <13458043813@163.com>
2018-11-29 14:07:25 +0800

    Laravel Passport API 认证使用小结 多表部分的内容  还要仔细研究细节

---
+++ app/Http/Controllers/App/RegisterController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/11/29
+ * Time: 12:49
+ */
+
+namespace App\Http\Controllers\App;
+
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RegisterController extends Controller
+{
+    public function index(Request $request){
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::namespace('App')->group(function () {
+    Route::get('register','RegisterController@index');
+});
